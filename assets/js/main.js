e266ec450ee9d4ff32582059c644f7181fe70d27
Caroline Vitóra <carolmelo007@hotmail.com>
2022-01-26 13:11:20 -0300

    nova página de confirmação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,10 @@
 function form(){
     const elForm = document.querySelector('form');
-    const elFechar = document.querySelector('.el-fechar');
-    const divConfirmacao = document.querySelector('.div-confirmacao');
-    const header = document.querySelector('header');
-    const main = document.querySelector('main');
 
     function telaDeConfirmacao(evento){
-        evento.preventDefault();        
-        document.getElementById('name').disabled = true;
-        document.getElementById('email').disabled = true;
-        document.getElementById('number').disabled = true;
-        document.querySelector('select').disabled = true;
-        divConfirmacao.style.display='block';
-        header.style.filter='blur(10px)';
-        main.style.filter='blur(10px)';
-
-    }
-    function fecharTela(){
-        elForm.submit();
+        evento.preventDefault();
+        window.location.href = "./confirmacao.html";
     }
     elForm.addEventListener('submit', telaDeConfirmacao);
-    elFechar.addEventListener('click', fecharTela);
 }
 form();
